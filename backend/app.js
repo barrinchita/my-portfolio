191f28995df704b15cac208e723538257bc96f14
Barrin Chita <BarrinChita@gmail.com>
2025-09-12 01:13:20 +0100

    db connection debugging

--- backend/app.js
+++ backend/app.js
@@ -57,6 +57,9 @@ app.use(cors(corsOptions))
 
 // creating a connection to mongoDb
 // the env variable mongo_uri is created from docker when starting the backend container
+
+console.log("db connection string: ", process.env.MONGO_URI);
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
